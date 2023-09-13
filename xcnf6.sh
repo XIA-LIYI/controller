e775ac1dbf54314b1eb4e9a7de55867385e77b57
XIA-LIYI <xialiyi@nus.edu.sg>
2023-09-13 23:30:42 +0800

    Update script

--- xcnf6.sh
+++ xcnf6.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #SBATCH --time=10
 #SBATCH --nodelist=xcnf6
+#SBATCH --ports=40000-41000
 
 srun ./client_xcnf6
--- xcnf7.sh
+++ xcnf7.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #SBATCH --time=10
 #SBATCH --nodelist=xcnf7
+#SBATCH --ports=40000-41000
 
 srun ./client_xcnf7
