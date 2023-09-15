9ded8b1210021eeb1a481db4990697eaf8302510
XIA-LIYI <xialiyi@nus.edu.sg>
2023-09-15 14:12:54 +0800

    A

--- test/test_7.go
+++ test/test_7.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	fmt.Println("Listening")
-	tcpAddr, _ := net.ResolveTCPAddr("tcp", "192.168.56.135:50120")
+	tcpAddr, _ := net.ResolveTCPAddr("tcp", "0.0.0.0:50120")
 	tcpListener, _ := net.ListenTCP("tcp", tcpAddr)
 	defer tcpListener.Close()
 	for {
