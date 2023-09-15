3d05c21692b9d64d11a2fea336222caba30cf6be
XIA-LIYI <e0261926@u.nus.edu>
2023-09-15 16:06:21 +0800

    Go

--- iperf/client1.sh
+++ iperf/client1.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 #SBATCH --time=1
 #SBATCH --nodelist=xcne1
-srun ./iperf -c 192.168.51.85 -p 5050 -i 2
+screen ./iperf -s -p 5050
+sleep 5
+screen ./iperf -c 192.168.51.85 -p 5050 -i 2
+
--- iperf/client2.sh
+++ iperf/client2.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #SBATCH --time=1
 #SBATCH --nodelist=xcne2
-srun ./iperf -c 192.168.51.84 -p 5050 -i 2
+screen ./iperf -s -p 5050
+sleep 5
+screen ./iperf -c 192.168.51.84 -p 5050 -i 2
