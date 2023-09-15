887ed9791e439aa1ae45d691c7145c3436e17799
XIA-LIYI <e0261926@u.nus.edu>
2023-09-15 16:08:08 +0800

    Go

--- iperf/client2.sh
+++ iperf/client2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #SBATCH --time=1
 #SBATCH --nodelist=xcne2
-screen ./iperf -s -p 5050
+bash -c screen ./iperf -s -p 5050
 sleep 5
-screen ./iperf -c 192.168.51.84 -p 5050 -i 2
+bash -c screen ./iperf -c 192.168.51.84 -p 5050 -i 2
