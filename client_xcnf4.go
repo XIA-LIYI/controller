3ad977e4a12dc381f01f22b1092d63281931b198
XIA-LIYI <e0261926@u.nus.edu>
2023-09-15 16:29:17 +0800

    Go

--- client_xcnf4.go
+++ client_xcnf4.go
@@ -11,13 +11,16 @@ import (
 var count int32 = 0
 var totalByte uint64 = 0
 var chans = []chan int {
-	make(chan int, 3),
-	// make(chan int),
+	make(chan int),
+	make(chan int),
 }
 
 func main() {
 	var tcpAddr *net.TCPAddr
 	tcpAddr, err := net.ResolveTCPAddr("tcp", "192.168.51.112:10000")
+	if (err != nil) {
+		fmt.Println(err)
+	}
 	conn, err := net.DialTCP("tcp", nil, tcpAddr)
 	if (err != nil) {
 		fmt.Println(err)
@@ -29,42 +32,36 @@ func main() {
 
 	startTime := time.Now()
 	for {
-		fmt.Println("waiting")
 		buf := make([]byte, 100)
 		num, _ := conn.Read(buf)
+		fmt.Println(num)
+		fmt.Println(string(buf)[:num])
 		content := string(buf)[:num]
-		fmt.Println(num, content)
 		if (content == "start") {
 			startTime = time.Now()
 			fmt.Println("Current number of connections is:", count)
 			for i := range chans {
-				chans[i] <- 0
+				chans[i] <- 0;
 			}
-			fmt.Println("All are released!")
 			continue
 		}
 		if (content == "stop") {
 			break
 		}
-		for {
-			addr, _ := net.ResolveTCPAddr("tcp", content)
-			newConn, err := net.DialTCP("tcp", nil, addr)
-			if (err != nil) {
-				fmt.Println(err)
-				continue
-			} else {
-				fmt.Println("connected!")
-			}
-			go onReceive(newConn)
-			go onSend(newConn, chans[count])
-			atomic.AddInt32(&count, 1)
-			break
+		tcpAddr, _ := net.ResolveTCPAddr("tcp", content)
+		newConn, err := net.DialTCP("tcp", nil, tcpAddr)
+		if (err != nil) {
+			fmt.Println(err)
 		}
+		go onReceive(newConn)
+		go onSend(newConn, chans[count])
+		atomic.AddInt32(&count, 1)
+
 	}
 	elapsedTime := uint64(time.Since(startTime) / time.Millisecond / 1000)
 	fmt.Println("Time consumed:", elapsedTime, "s")
-	speed := totalByte / 1000 / elapsedTime * 8 / 1000
-	fmt.Println("Speed is:", speed, "Mbps")
+	speed := totalByte / 1000 / elapsedTime * 8
+	fmt.Println("Time consumed:", speed, "Kbps")
 
 	// 控制台聊天功能加入
 	// for {
@@ -80,7 +77,7 @@ func main() {
 
 func listen() {
 	fmt.Println("Listening")
-	tcpAddr, _ := net.ResolveTCPAddr("tcp", "0.0.0.0:5050")
+	tcpAddr, _ := net.ResolveTCPAddr("tcp", "0.0.0.0:10000")
 	tcpListener, _ := net.ListenTCP("tcp", tcpAddr)
 	defer tcpListener.Close()
 	for {
@@ -90,7 +87,7 @@ func listen() {
 		}
 		fmt.Println("A client connected:" + tcpConn.RemoteAddr().String())
 		go onReceive(tcpConn)
-	    go onSend(tcpConn, chans[count])
+		go onSend(tcpConn, chans[count])
 		atomic.AddInt32(&count, 1)
 	}
 }
@@ -110,25 +107,25 @@ func onMessageRecived(conn *net.TCPConn) {
 }
 
 func onReceive(conn *net.TCPConn) {
-	fmt.Println("start receiving")
-	buf := make([]byte, 12500000 * 2)
 	for {
+		buf := make([]byte, 12500000)
 		num, _ := conn.Read(buf)
 		atomic.AddUint64(&totalByte, uint64(num))
+		fmt.Println(num)
 	}
 
 }
 
 func onSend(conn *net.TCPConn, ch chan int) {
-	fmt.Println("start sending")
 	<- ch
-	ticker := time.NewTicker(time.Second / 100)
+	ticker := time.NewTicker(time.Second / 10)
 	defer ticker.Stop()
 	content := make([]byte, 12500000)
 	for {
 		// fmt.Println("start sending")
 		for {
 			<- ticker.C
+			fmt.Println("tick.")
 			conn.Write(content)
 		}
 	}
