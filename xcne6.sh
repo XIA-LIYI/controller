f06497c43a2111f588c58ebfb5ac56ebb3c1d8e7
XIA-LIYI <xialiyi@nus.edu.sg>
2023-09-18 14:48:03 +0800

    A

--- xcne6.sh
+++ xcne6.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #SBATCH --time=10
-#SBATCH --partition=medium
+#SBATCH --partition=long
 #SBATCH --nodelist=xcne6
 #SBATCH --ntasks=1 --cpus-per-task=20
 
--- xcne7.sh
+++ xcne7.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #SBATCH --time=10
-#SBATCH --partition=medium
+#SBATCH --partition=long
 #SBATCH --nodelist=xcne7
 #SBATCH --ntasks=1 --cpus-per-task=20
 
