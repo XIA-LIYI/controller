09070cbb9b14ea3340d7230a70faed8ca840a4bb
XIA-LIYI <xialiyi@nus.edu.sg>
2023-09-18 14:53:17 +0800

    A

--- run.sh
+++ run.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 sbatch xcne0.sh
+sleep 5
 sbatch xcne1.sh
+sleep 5
 sbatch xcne2.sh
+sleep 5
 sbatch xcne3.sh
+sleep 5
 sbatch xcne4.sh
+sleep 5
 sbatch xcne5.sh
+sleep 5
 sbatch xcne6.sh
+sleep 5
 sbatch xcne7.sh
