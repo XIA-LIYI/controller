515a7fd8a22f715872ff6ef95eca1c67bb447fd0
XIA-LIYI <xialiyi@nus.edu.sg>
2023-09-21 16:05:37 +0800

    Update

--- run.sh
+++ run.sh
@@ -1,30 +1,9 @@
-#!/bin/bash
-sbatch xcne0.sh
-sleep 5
-sbatch xcne1.sh
-sleep 5
-# sbatch xcne2.sh
-# sleep 5
-sbatch xcne3.sh
-sleep 5
-sbatch xcne4.sh
-sleep 5
-sbatch xcne5.sh
-sleep 5
-sbatch xcne6.sh
-sleep 5
-sbatch xcne7.sh
-sleep 5
-sbatch xgph10.sh
-sleep 5
-sbatch xgph11.sh
-sleep 5
-sbatch xgph12.sh
-sleep 5
-sbatch xgph13.sh
-sleep 5
-sbatch xgph14.sh
-sleep 5
-sbatch xgph15.sh
-sleep 5
-sbatch xgph16.sh
+#!/bin/sh
+#SBATCH --time=10
+#SBATCH --partition=long
+SBATCH --nodes=6
+SBATCH --nodelist=xcne2,xcne3,xcne4,xcne5,xcne6,xcne7
+#SBATCH --ntasks=25 --cpus-per-task=1
+
+srun -n 25 ./client
+# srun -N 2 -n 2 ./client
--- xcne/xcne.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#SBATCH --time=10
-#SBATCH --partition=long
-#SBATCH --nodes=6
-#SBATCH --nodelist=xcne2,xcne3,xcne4,xcne5,xcne6,xcne7
-#SBATCH --ntasks=6 --cpus-per-task=1
-
-srun -n 6 ./client
-# srun -N 2 -n 2 ./client
