aeec21811a462b0b01cf8f400cabffda518168ee
XIA-LIYI <xialiyi@nus.edu.sg>
2023-09-21 16:17:10 +0800

    A

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#SBATCH --time=10
-#SBATCH --partition=long
-#SBATCH --nodes=25
-#SBATCH --ntasks=25 --cpus-per-task=1
-#SBATCH --ntasks-per-node=1
-
-srun -n 25 ./client
-# srun -N 2 -n 2 ./client
