da6d252f4fc0d713bc4d4c745a47a3ab5c41c74e
XIA-LIYI <xialiyi@nus.edu.sg>
2023-09-21 15:58:36 +0800

    A

--- xcne/xcne.sh
+++ xcne/xcne.sh
@@ -2,8 +2,8 @@
 #SBATCH --time=10
 #SBATCH --partition=long
 #SBATCH --nodes=2
-#SBATCH --nodelist=xcne2,xcne3,xcne4,xcne5,xcne6,xcne7
-#SBATCH --ntasks= --cpus-per-task=1
+#SBATCH --nodelist=xcne2,xcne3,xcne4,xcne5,xcne6,xcne7,xcne7
+#SBATCH --ntasks=7 --cpus-per-task=1
 
 srun -n 8 ./client
 # srun -N 2 -n 2 ./client
