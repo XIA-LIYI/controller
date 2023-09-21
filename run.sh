767da3894e071f8530560fcb5b6e4bbb260cd32c
XIA-LIYI <xialiyi@nus.edu.sg>
2023-09-21 16:08:02 +0800

    A

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #SBATCH --time=10
 #SBATCH --partition=long
-# SBATCH --nodes=6
-# SBATCH --nodelist=xcne2,xcne3,xcne4,xcne5,xcne6,xcne7
+#SBATCH --nodes=25
 #SBATCH --ntasks=25 --cpus-per-task=1
+#SBATCH --ntasks-per-node=1
 
 srun -n 25 ./client
 # srun -N 2 -n 2 ./client
